17480b212136dcaf5179546a195c485856515e82
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-02-10 15:28:01 -0500

    Added a core.bee2 module for interfacing to BEE2 hardware.
    Also, updated backends.sma to use core.bee2 and the core.loggers
    logging functionality.

--- backends/sma.py
+++ backends/sma.py
@@ -15,14 +15,17 @@ running on the DDS.
 """
 
 
-import logging
 from time import sleep
 from struct import Struct
 
 from numpy import array
 
-from corr.katcp_wrapper import FpgaClient
 
+from core.bee2 import BEE2Client
+from core.loggers import (
+    debug, info, warning, 
+    critical, error,
+)
 from backends.basic import (
     BasicCorrelationProvider,
     BasicRequestHandler,
@@ -43,6 +46,7 @@ class BEE2CorrelationProvider(BasicCorrelationProvider):
     over UDP packets to registered subscribers. See 'backends.basic.
     BasicCorrelationProvider' for more detail."""
 
+    @debug
     def __init__(self, server, include_baselines, 
                  bee2_host, bee2_port, lags=32,
                  bof='bee2_calib_corr.bof'):
@@ -51,11 +55,11 @@ class BEE2CorrelationProvider(BasicCorrelationProvider):
         BasicCorrelationProvider.__init__(self, server, include_baselines, lags)
         self.bee2_host = bee2_host
         self.bee2_port = bee2_port
-        self.bee2 = FpgaClient(bee2_host, port=bee2_port)
+        self.bee2 = BEE2Client(bee2_host, port=bee2_port)
         self.bee2._connected.wait()
         self._program(bof)
-        self.bee2.write_int('start', 1)
 
+    @debug
     def _program(self, bof):
         """ Update the list of available bitstreams and program
         the  BEE2 corner chip with the requested image."""
@@ -69,6 +73,7 @@ class BEE2CorrelationProvider(BasicCorrelationProvider):
             self.logger.error(err_msg)
             raise BEE2BorphError(err_msg)
 
+    @info
     def correlate(self):
         """ This overloads 'BasicCorrelationProvider.correlate'
         (which does nothing) and enables/resets correlations on
@@ -78,22 +83,14 @@ class BEE2CorrelationProvider(BasicCorrelationProvider):
         self.logger.debug('correlate()')
         integration_time = self.server._integration_time
         self.logger.info("correlating for %0.2f seconds" %integration_time)
-        self.bee2.write_int('hb_cntto', integration_time+1)
         for baseline in self._include_baselines:
-            raw = self.bee2.read('corr_out%d' %(int(baseline[1])-1), 128)
-            self._correlations[baseline] = array(CORR_OUT.unpack(raw))
-            self.logger.info('baseline %s, mean %d' %(baseline, self._correlations[baseline].mean()))
-        self.bee2.write_int('corr_record', 0)
-        self.bee2.write_int('corr_en', 0)
-        self.bee2.write_int('corr_rst', 1)
-        self.bee2.write_int('corr_rst', 0)
-        self.bee2.write_int('corr_en', 1)
-        sleep(integration_time+1)
-        self.bee2.write_int('corr_record', 1)
+            pass
+            #self.logger.info('baseline %s, mean %d' %(baseline, self._correlations[baseline].mean()))
 
 
 class SubmillimeterArrayTCPServer(BasicTCPServer):
-    
+
+    @debug
     def __init__(self, address, handler=BasicRequestHandler,
                  correlator=BEE2CorrelationProvider,
                  n_antennas=8, correlator_lags=32, 
---
+++ core/bee2.py
@@ -0,0 +1,103 @@
+"""
+Classes for communicating with a BEE2 tcpborphserver instance
+
+Note: BEE2Client was inspired by the 'corr' package's FpgaClient 
+"""
+
+
+import logging
+import struct
+
+from katcp import BlockingClient, Message
+
+from loggers import debug, info
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
+class BEE2Client(BlockingClient):
+    """Interface to a tcpborphserver instance running on BEE2
+    """
+
+    @debug
+    def __init__(self, host, port=7147, tb_limit=20, timeout=10.0):
+        self._timeout = timeout
+        katcp_logger = logging.Logger("katcp")
+        katcp_logger.addHandler(NullHandler())
+        BlockingClient.__init__(self, host, port, tb_limit=tb_limit,
+                                timeout=timeout, logger=katcp_logger)
+        self.start(daemon=True)
+
+    @debug
+    def _request(self, name, *args):
+        request = Message.request(name, *args)
+        reply, informs = self.blocking_request(request, keepalive=True)
+        if reply.arguments[0] != Message.OK:
+            self._logger.error("Request %s failed.\n  Request: %s\n  Reply: %s."
+                               % (request.name, request, reply))
+            raise RuntimeError("Request %s failed.\n  Request: %s\n  Reply: %s."
+                               % (request.name, request, reply))
+        return reply, informs
+
+    @debug
+    def listbof(self):
+        reply, informs = self._request("listbof")
+        return [i.arguments[0] for i in informs]
+
+    @info
+    def progdev(self, device_name):
+        reply, informs = self._request("progdev", device_name)
+        return reply.arguments[0]
+
+    @debug
+    def listdev(self):
+        reply, informs = self._request("listdev")
+        return [i.arguments[0] for i in informs]
+
+    @debug
+    def _read(self, device_name, size, offset=0):
+        reply, informs = self._request("read", device_name, str(offset), str(size))
+        return reply.arguments[1]
+
+    @debug
+    def _write(self, device_name, data, offset=0):
+        self._request("write", device_name, str(offset), data)
+
+    @info
+    def regread(self, device_name, signed=False):
+        if signed:
+            format = ">i"
+        else:
+            format = ">I"
+        data = self._read(device_name, 4, 0)
+        return struct.unpack(format, data)[0]
+
+    @debug
+    def regwrite(self, device_name, integer, signed=False):
+        if signed:
+            format = ">i"
+        else:
+            format = ">I"
+        data = struct.pack(format, integer)
+        self._write(device_name, data, 0)
+
+    @info
+    def bramread(self, device_name, size, offset=0, signed=True):
+        if signed:
+            format = ">%di" %size
+        else:
+            format = ">%dI" %size
+        data = self._read(device_name, size*4, offset=offset)
+        return struct.unpack(format, data)
+
+    @debug
+    def bramwrite(self, device_name, integers, offset=0, signed=True):
+        if signed:
+            format = ">%di" %len(integers)
+        else:
+            format = ">%dI" %len(integers)
+        data = struct.pack(format, *integers)
+        self._write(device_name, data, offset=offset)
