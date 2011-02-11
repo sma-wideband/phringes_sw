8bb3bfc1cce6f3f8370a81ac1f645acf48cb7d36
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-02-11 14:31:49 -0500

    Added a simple script for stopping the server process.

---
+++ stop_sma.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--host", action="store",
+                  dest="host", default="localhost",
+                  help="stop the server running on HOST, defaults to 'localhost'",
+                  metavar="HOST")
+parser.add_option("--port", action="store", type="int",
+                  dest="port", default=59999,
+                  help="stop the server running on PORT, defaults to 59999",
+                  metavar="PORT")
+(options, args) = parser.parse_args()
+
+
+from backends.basic import BasicTCPClient
+
+sma = BasicTCPClient(options.host, options.port)
+sma.shutdown()
