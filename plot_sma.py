7e11a8067ba6e21163f28ed19897edcec705b93b
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-02-20 03:01:21 +0000

    The SMA correlator plotting script now shows all baselines.

--- plot_sma.py
+++ plot_sma.py
@@ -4,7 +4,6 @@
 import logging
 from time import time
 from struct import Struct
-from Queue import Empty as QueueEmpty
 from Tkinter import Tk, StringVar, Frame,\
      OptionMenu, Label, Button, BOTTOM, LEFT, RIGHT, BOTH
 
@@ -20,7 +19,7 @@ from phringes.plotting.rtplot import RealTimePlot
 
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.WARNING)
 
 
 root = Tk()
@@ -28,7 +27,7 @@ root.iconify()
 root.wm_title("Correlator Monitor")
 
 frame = Frame(root)
-frame.grid()#(fill=BOTH, expand=1)
+frame.pack(fill=BOTH, expand=1)
 
 correlator = sma.BEE2CorrelatorClient('0.0.0.0', 8332)
 server = sma.SubmillimeterArrayClient('128.171.116.126', 59999)
@@ -40,45 +39,42 @@ server.start_correlator()
 
 f = arange(-8, 8)
 corr = RealTimePlot(master=frame, mode='replace', ylim=[-2**31, 2**31], xlim=[f.min(), f.max()])
-corr.plot(f, arange(-8, 8), linewidth=3)
-corr.tkwidget.grid()#pack(fill=BOTH, expand=1)
+corr.tkwidget.pack(fill=BOTH, expand=1)
 
 def quit_mon():
     server.unsubscribe(correlator.host, correlator.port)
-    correlator.stop()
     root.quit()
 
 quit = Button(master=frame, text='Quit', command=quit_mon)
-quit.grid()#(side=BOTTOM)
+quit.pack(side=BOTTOM)
 
-def update_plots(widget, total_updates):
+
+def update_plots(widget, baselines):
     try:
         corr_time, left, right, current, total, lags = correlator.get_correlation()
         baseline = left, right
-        correlator.logger.info('(%d) received lags for baseline %s' % (total_updates, repr(baseline)))
+        correlator.logger.info('received baseline %s' % repr(baseline))
     except NoCorrelations:
-        widget.after_idle(update_plots, widget, total_updates)
+        widget.after(1, update_plots, widget, baselines)
         return
-    if 1 in baseline:
-        print 'found baseline with 1 %s' %repr(abs(lags))
-        corr.update(f, abs(lags))
-        #if total_updates==0 and 1 in baseline:
-            #corr.plot(f, abs(lags), linewidth=3)
-            #corr.axes.grid()
-            #corr.axes.set_ylabel('Correlation Function', size='large')
-            #corr.axes.set_xlabel('Lag', size='large')
-        #elif 1 in baseline:
-            #corr.update(f, abs(lags))
-            #if total_updates%100==0:
-            #    corr.axes.set_title("FPS: %0.2f, Integration time: %0.2f ms"\
-            #                        %((total_updates/(time()-start_time)), (n/(800000.))))
-        widget.update()
-        total_updates += 1
-    widget.after_idle(update_plots, widget, total_updates)
+    if baseline not in baselines.keys():
+        corr.axes.grid()
+        corr.axes.set_xlabel('Lag', size='large')
+        corr.axes.set_ylabel('Correlation Function', size='large')
+        line = corr.plot(f, abs(lags), linewidth=1, label=repr(baseline))[0]
+        baselines[baseline] = line
+    else:
+        corr.axes.legend()
+        line = baselines[baseline]
+        corr.update_line(line, f, abs(lags))
+        corr.draw() # must be called since we're not using update(..)
+    widget.update()
+    widget.after_idle(update_plots, widget, baselines)
+
 
 root.update()
 root.geometry(frame.winfo_geometry())
-root.deiconify()
+root.after_idle(update_plots, root, {})
 
-root.after_idle(update_plots, root, 0)
+root.deiconify()
 root.mainloop()
