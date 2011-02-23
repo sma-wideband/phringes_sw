c71ce3ecf5d3e4f6be5769fe8a61b45e341a9535
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-02-23 23:15:21 +0000

    Moved all scripts to a 'scripts' folder and added a setup.py installer.
    $ python setup.py install
    
    will build all modules, including extensions, and install them along
    with the scripts, by default to /usr/local.

--- setup.py
+++ setup.py
@@ -9,4 +9,6 @@ setup(
     description = 'Python software for interfacing to the PHRINGES system',
     packages = ['phringes', 'phringes.core', 'phringes.backends', 'phringes.plotting'],
     ext_modules = [Extension('phringes.backends._dds', ['src/dds.c', 'src/dDS_clnt.c', 'src/dDS_xdr.c'])],
+    scripts=['scripts/init_sma.py', 'scripts/serve_sma.py', 'scripts/stop_sma.py', 'scripts/plot_sma.py',
+             'scripts/dbetcp.py'],
     )
