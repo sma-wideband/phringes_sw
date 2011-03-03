b57b00a96775562f28c17c13e9de3577a7b507e2
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-03-03 23:35:01 +0000

    Changed some logging in the correlator from INFO to DEBUG.

--- phringes/backends/basic.py
+++ phringes/backends/basic.py
@@ -152,8 +152,8 @@ class BasicCorrelationProvider:
             current += 1
             pkt = header + data
             pkt_len = len(pkt) + SHORT_SIZE
-            self.logger.info('packet: %r' % pkt)
-            self.logger.info('sending {0} bytes of data, {1} bytes total'.format(len(data), pkt_len))
+            self.logger.debug('packet: %r' % pkt)
+            self.logger.debug('sending {0} bytes of data, {1} bytes total'.format(len(data), pkt_len))
             for subscriber in self.subscribers:
                 udp_sock = socket(AF_INET, SOCK_DGRAM)
                 udp_sock.sendto(SHORT.pack(pkt_len)+pkt, subscriber)
