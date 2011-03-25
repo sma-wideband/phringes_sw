36dfd7a17fa35884539b0fdeac81eb0b968523c9
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-03-25 07:49:47 -1000

    Added a --remote option to plot_vlbi.py, and reorganized the GUI a bit.

--- scripts/plot_vlbi.py
+++ scripts/plot_vlbi.py
@@ -3,6 +3,7 @@
 
 import logging
 from optparse import OptionParser
+from socket import gethostbyname_ex, gethostname
 
 parser = OptionParser()
 parser.add_option("-q", "--quiet", action="store_false",
@@ -11,6 +12,9 @@ parser.add_option("-q", "--quiet", action="store_false",
 parser.add_option("-v", "--debug", action="store_true",
                   dest="debug", default=False,
                   help="print DEBUG messages to stdout")
+parser.add_option("-r", "--remote", action="store_true",
+                  dest="remote", default=False,
+                  help="plotting is remote, using an SSH tunnel")
 parser.add_option("--block", action="store",
                   dest="block", default="high",
                   help="start the correlator on BLOCK, can be 'high' or 'low' "
@@ -35,15 +39,17 @@ logger.setLevel(LEVEL)
 logger.addHandler(console)
 
 
-if options.block=='high':
+if options.remote:
     server_host = '0.0.0.0'
-    server_port = 59999
     listen_host = '0.0.0.0'
+else:
+    server_host = '128.171.116.126'
+    listen_host = gethostbyname_ex(gethostname())[2][0]
+if options.block=='high':
+    server_port = 59999
     listen_port = 8333
 elif options.block=='low':
-    server_host = '0.0.0.0'
     server_port = 59998
-    listen_host = '0.0.0.0'
     listen_port = 8332
 else:
     raise ValueError, "Block option must be either 'high' or 'low'!"
@@ -53,11 +59,12 @@ from math import pi
 from time import time
 from struct import Struct
 from datetime import datetime, timedelta
-from socket import gethostbyname, gethostname
+from tkSimpleDialog import askfloat
 from Tkinter import (
-    Tk, StringVar, Frame, LabelFrame, 
-    Entry, OptionMenu, Label, Button, 
+    Tk, StringVar, BooleanVar, Frame, LabelFrame, 
+    Entry, OptionMenu, Label, Button, Checkbutton,
     TOP, BOTTOM, LEFT, RIGHT, BOTH,
+    N, S, E, W
     )
 
 from numpy.fft import fft
@@ -86,69 +93,88 @@ window = Frame(root)
 window.pack(side=RIGHT, fill=BOTH, expand=1)
 
 plots = Frame(window)
-plots.pack(side=LEFT, fill=BOTH, expand=1)
+plots.grid(row=0, column=0, columnspan=3, sticky=(N, S, E, W))#pack(side=LEFT, fill=BOTH, expand=1)
 
-buttons = LabelFrame(window, text='Plotting')
-buttons.pack(side=TOP, fill=BOTH, expand=1)
+plotting = LabelFrame(window, text='Plotting')
+plotting.grid(row=1, column=0, sticky=(N, S, E, W))#pack(side=TOP, fill=BOTH, expand=1)
 
-mandc = LabelFrame(window, text='Monitor and Control')
-mandc.pack(side=BOTTOM, fill=BOTH, expand=1)
+monitor = LabelFrame(window, text='Monitor')
+monitor.grid(row=1, column=1, sticky=(N, S, E, W))#pack(side=BOTTOM, fill=BOTH, expand=1)
+
+control = LabelFrame(window, text='Control')
+control.grid(row=1, column=2, sticky=(N, S, E, W))#pack(side=BOTTOM, fill=BOTH, expand=1)
+
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=1)
+window.rowconfigure(2, weight=1)
+
 
 server = sma.SubmillimeterArrayClient(server_host, server_port)
+if options.remote:
+    client = server
+else:
+    client = sma.BEE2CorrelatorClient(listen_host, listen_port)
 try:
     server.subscribe(listen_host, listen_port) # the server's local UDP client
 except:
     pass
 server.start_correlator()
 
+
 f = arange(-7, 8)
 l = arange(-8, 8)
 phase_limits = -pi*1.5, pi*1.5
 
-
 lags = RealTimePlot(master=plots, mode='replace', xlim=[f.min(), f.max()])
-lags.tkwidget.grid(row=0, column=0)#, fill=BOTH, expand=1)
+lags.tkwidget.grid(row=0, column=0, sticky=(N, S, E, W))
 
 corr = RealTimePlot(master=plots, mode='replace', ylim=phase_limits, xlim=[f.min(), f.max()])
-corr.tkwidget.grid(row=0, column=1)#, fill=BOTH, expand=1)
+corr.tkwidget.grid(row=0, column=1, sticky=(N, S, E, W))
 
 hist = RealTimePlot(master=plots, xspan=timedelta(minutes=10), xpoints=40, ylim=phase_limits)
-hist.tkwidget.grid(row=1, column=0)#, fill=BOTH, expand=1)
+hist.tkwidget.grid(row=1, column=0, sticky=(N, S, E, W))
 
-maghist = RealTimePlot(master=plots, xspan=timedelta(minutes=10), xpoints=40, ylim=[0, 80])
-maghist.tkwidget.grid(row=1, column=1)#, fill=BOTH, expand=1)
+maghist = RealTimePlot(master=plots, xspan=timedelta(minutes=10), xpoints=40, ylim=[0, 160])
+maghist.tkwidget.grid(row=1, column=1, sticky=(N, S, E, W))
 
+plots.columnconfigure(0, weight=1)
+plots.columnconfigure(1, weight=1)
+plots.columnconfigure(2, weight=1)
+plots.rowconfigure(0, weight=1)
+plots.rowconfigure(1, weight=1)
+
+
+button_width = 8
 
 def quit_mon():
     server.unsubscribe(listen_host, listen_port)
     server.stop_correlator()
     root.quit()
 
-quit = Button(master=buttons, text='Quit', command=quit_mon)
-quit.grid()#pack(side=TOP)
+quit = Button(master=control, text='Quit', command=quit_mon, width=button_width)
+quit.grid(row=0, column=1)#pack(side=TOP)
 
 
 def update_integration_time():
-    server.stop_correlator()
-    server.set_integration_time(float(itime.get()))
-    itime.set(server.get_integration_time())
-    server.start_correlator()
+    cur_itime = server.get_integration_time()
+    itime = askfloat('Set integration time', 'Seconds: ', parent=root, initialvalue=cur_itime)
+    if itime:
+        server.stop_correlator()
+        server.set_integration_time(itime)
+        server.start_correlator()
 
-itime = StringVar()
-itime.set(server.get_integration_time())
-itime_entry = Entry(master=mandc, textvariable=itime, width=24)
-itime_entry.grid(row=0, column=0)
-
-update_itime = Button(master=mandc, text='Update', command=update_integration_time)
-update_itime.grid(row=0, column=1)
+update_itime = Button(master=control, text='Update', command=update_integration_time, width=button_width)
+update_itime.grid(row=1, column=1)
 
 
-excludes = [3, 4]
+show_baseline = {}
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 def update_plots(widget, baselines, statusbar):
     try:
         corr_time, left, right, current, total, \
-            lag, visibility, phase_fit, delay, phase = server.get_correlation()
+            lag, visibility, phase_fit, delay, phase = client.get_correlation()
         baseline = left, right
         phases = angle(visibility)
         residuals = phases - phase_fit
@@ -156,17 +182,20 @@ def update_plots(widget, baselines, statusbar):
     except NoCorrelations:
         widget.after(1, update_plots, widget, baselines, statusbar)
         return 
-    if left in excludes or right in excludes:
-        #logger.info("{0}-{1} ignored".format(*baseline))
-        widget.after(1, update_plots, widget, baselines, statusbar)
-        return
     if baseline not in baselines.keys():
+        label_size = 'x-large'
         corr.axes.grid()
+        corr.axes.set_xlabel('Frequency (MHz)', size=label_size)
+        corr.axes.set_ylabel('Phase (rad)', size=label_size)
         hist.axes.grid()
+        hist.axes.set_xlabel('Time (local)', size=label_size)
+        hist.axes.set_ylabel('Phase (rad)', size=label_size)
         lags.axes.grid()
+        lags.axes.set_xlabel('Lag', size=label_size)
+        lags.axes.set_ylabel('Uncalibrated Amplitude', size=label_size)
         maghist.axes.grid()
-        #corr.axes.set_xlabel('Lag', size='large')
-        #corr.axes.set_ylabel('Correlation Function', size='large')
+        maghist.axes.set_xlabel('Time (local)', size=label_size)
+        maghist.axes.set_ylabel('Uncalibrated Amplitude (dB)', size=label_size)
         lags_line = lags.plot(
             l, abs(lag), '%s-' % colors[current % len(colors)], 
             linewidth=1, label=repr(baseline)
@@ -191,10 +220,15 @@ def update_plots(widget, baselines, statusbar):
         hist.figure.autofmt_xdate()
         maghist.figure.autofmt_xdate()
         baselines[baseline] = lags_line, phase_line, fit_line, phist_line, mag_line
-        status = StringVar()
-        statusbar[baseline] = status
-        Label(master=mandc, textvariable=status).grid(columnspan=2)
+        statusbar[baseline] = StringVar()
+        Label(master=monitor, textvariable=statusbar[baseline]).grid(columnspan=2)
+        show = BooleanVar()
+        show.set(True)
+        show_baseline[baseline] = show
+        Checkbutton(master=plotting, text='%d-%d'%baseline, variable=show).grid(row=0, column=current)
     else:
+        for line in baselines[baseline]:
+            line.set_visible(show_baseline[baseline].get())
         corr.axes.legend()
         lags_line, phase_line, fit_line, phist_line, mag_line = baselines[baseline]
         lags.update_line(lags_line, l, abs(lag))
@@ -202,6 +236,7 @@ def update_plots(widget, baselines, statusbar):
         corr.update_line(fit_line, f, phase_fit)
         hist.update_line(phist_line, datetime.fromtimestamp(corr_time), phase)
         maghist.update_line(mag_line, datetime.fromtimestamp(corr_time), 10*log10(abs(lag).max()))
+            
     status = statusbar[baseline]
     mean = 100*abs(lag).mean()/(2**31)
     span = 100.*(abs(lag).max() - abs(lag).min())/(2**31)
@@ -213,6 +248,8 @@ def update_plots(widget, baselines, statusbar):
     #logger.info('{0}-{1} >> phase={2:8.2f}deg, mean/span={3:.2f}/{4:.2f}'.format(left, right, phase*(180/pi), mean, span))
     if current == total-1:
         widget.update()
+        #root.geometry(window.winfo_geometry())
+        #root.deiconify()
         #logger.info('update in')
     widget.after_idle(update_plots, widget, baselines, statusbar)
 
